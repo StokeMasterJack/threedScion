0307f8c8ae8fb177159d3951de0690f78dee021c
David Ford <dford@smart-soft.com>
2012-11-14 23:55:36 -0800

    Deleted unused file

--- nfc/src/java/c3i/smartClient/client/skins/viewSelectors/ViewSelectorFactory.java
+++
@@ -1,9 +0,0 @@
-package c3i.smartClient.client.skins.viewSelectors;
-
-import c3i.smartClient.client.model.ThreedSession;
-
-public interface ViewSelectorFactory {
-
-    ViewSelector create(ThreedSession threedSession);
-
-}
