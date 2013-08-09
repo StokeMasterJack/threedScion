70a06d06cb6ea8f71855071b6d84beb1f9a47247
David Ford <dford@smart-soft.com>
2013-08-08 19:39:59 -0700

    Fix ie console.log bug

--- nfc/src/java/c3i/smartClient/client/ExportJavaScriptEntryPoint.java
+++ nfc/src/java/c3i/smartClient/client/ExportJavaScriptEntryPoint.java
@@ -2,16 +2,26 @@ package c3i.smartClient.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import org.timepedia.exporter.client.ExporterUtil;
-import java.util.logging.Level;import java.util.logging.Logger;
+import smartsoft.util.gwt.client.IELogFix;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ExportJavaScriptEntryPoint implements EntryPoint {
+
+    public ExportJavaScriptEntryPoint() {
+        IELogFix.installFix();
+    }
 
     public void onModuleLoad() {
 
         try {
+            System.err.println("Before ExporterUtil.exportAll()");
             ExporterUtil.exportAll();
+            System.err.println("After ExporterUtil.exportAll()");
         } catch (Exception e) {
-            log.log(Level.INFO, "Problem with gwt-export: " + e.toString());
+            log.log(Level.SEVERE, "Problem with gwt-export: " + e.toString());
+            System.err.println("Problem with gwt-export: " + e.toString());
             e.printStackTrace();
         }
 
@@ -19,6 +29,7 @@ public class ExportJavaScriptEntryPoint implements EntryPoint {
             doOnThreedReady();
         } catch (Exception e) {
             log.log(Level.INFO, "Error in initThreed: " + e.toString());
+            System.err.println("Error in initThreed: " + e.toString());
             e.printStackTrace();
         }
 
@@ -29,11 +40,11 @@ public class ExportJavaScriptEntryPoint implements EntryPoint {
             try {
                 $wnd.onThreedReady();
             } catch (e) {
-                console.error("Error calling onThreedReady");
-                console.error(e);
+                if (window.console) console.error("Error calling onThreedReady");
+                if (window.console) console.error(e);
             }
         } else {
-            console.log("no onThreedReady method to call")
+            if (window.console) console.log("no onThreedReady method to call")
         }
     }-*/;
 
